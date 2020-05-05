32261c1ff6482276358ffe8b364af9aee5631ae2
aifa21 <37891597+aifa21@users.noreply.github.com>
2020-05-06 00:57:02 +0600

    Add files via upload

---
+++ Add Odd or Subtract Even.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b;
+        cin>>a>>b;
+        if(a==b)
+        {
+            cout<<"0"<<endl;
+        }
+        else if((a<b&&a%2==0&&b%2==0)||(a<b&&a%2!=0&&b%2!=0))
+        {
+             cout<<"2"<<endl;
+        }
+        else if((a>b&&a%2==0&&b%2!=0)||(a>b&&a%2!=0&&b%2==0))
+        {
+             cout<<"2"<<endl;
+        }
+        else{
+            cout<<"1"<<endl;
+        }
+    }
+}
