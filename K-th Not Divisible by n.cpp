1b3032a25e6290c2b7a501bdda8ecc9aa085613e
aifa21 <37891597+aifa21@users.noreply.github.com>
2020-05-17 21:19:20 +0600

    Add files via upload

---
+++ K-th Not Divisible by n.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+    long long int n,k;
+    cin>>n>>k;
+   long long int nn,kk;
+   nn=n-1;
+   kk=k-1;
+   int p=kk/nn;
+   cout<<k+p<<endl;
+
+
+
+    }
+    return 0;
+}
